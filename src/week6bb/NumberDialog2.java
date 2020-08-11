e7ef7935376910f33af032e49defcbef4be5c3e7
liw-Boonyarit <liw_2588@hotmail.com>
2020-08-11 17:31:25 +0700

    first commit

---
+++ src/week6bb/NumberDialog2.java
@@ -0,0 +1,12 @@
+package week6bb;
+
+import javax.swing.JOptionPane;
+
+public class NumberDialog2 {
+	public static void main(String[]args) {
+		int creditDay = 30;
+		JOptionPane.showMessageDialog(null, " " + creditDay);
+		JOptionPane.showMessageDialog(null, "Every bill is due in"+ creditDay + " day ");
+		JOptionPane.showMessageDialog(null, "Thank you ");
+	}
+}
---
+++ src/week6bb/data101.java
@@ -0,0 +1,11 @@
+package week6bb;
+
+import javax.swing.JOptionPane;
+
+public class data101 {
+	public static void main(String[]args) {
+		System.out.println("First Java application");
+		JOptionPane.showMessageDialog(null, "Hello My Cat");
+	}
+	
+}
